f14ca8264dc475fb8669ec2daaa0ffb3ab706d2e
CleSucre <julien.thomas.4321@gmail.com>
2022-11-15 14:03:38 +0100

    Add ft_strdup

--- libft.h
+++ libft.h
@@ -61,4 +61,5 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+char	*ft_strdup(const char *s1);
 #endif
