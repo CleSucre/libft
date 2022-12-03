f2ae26edbcee0acefa6a9242d6c5653f6a987c99
Julien Thomas <jthomas@vmi1065519.contaboserver.net>
2022-12-03 01:42:27 +0100

    trying tp fix lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jthomas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 16:54:00 by jthomas           #+#    #+#             */
-/*   Updated: 2022/11/29 05:17:32 by jthomas          ###   ########.fr       */
+/*   Updated: 2022/12/03 01:36:12 by jthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*res;
 
-	res = NULL;
-	if (lst)
+	if (!lst)
+		return (NULL);
+	res = ft_lstnew(f(lst->content));
+	if (res == NULL)
 	{
-		res = ft_lstnew(f(lst->content));
-		if (!res)
-		{
-			ft_lstclear(&res, del);
-			return (NULL);
-		}
-		if (lst->next)
-			res->next = ft_lstmap(lst->next, f, del);
+		ft_lstclear(&res, del);
+		return (NULL);
 	}
+	res->next = ft_lstmap(lst->next, f, del);
 	return (res);
 }
