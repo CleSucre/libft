78129990e07e36350b2bdc5262ed2e5e91c0710e
CleSucre <julien.thomas.4321@gmail.com>
2023-03-05 09:19:14 +0100

    Update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -16,6 +16,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+#define WHITESPACES " \t\n\r\f\v"
+#define SPACE " "
+#define TAB "\t"
+#define NEWLINE "\n"
+#define CARRIAGE_RETURN "\r"
+#define FORM_FEED "\f"
+#define VERTICAL_TAB "\v"
+
 typedef struct s_list
 {
 	void			*content;
@@ -57,21 +65,24 @@ size_t	ft_nbrlen(long n);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-int		ft_putchar_fd(char c, int fd);
-int		ft_putstr_fd(char *s, int fd);
-int		ft_putendl_fd(char *s, int fd);
-int		ft_putnbr_fd(int n, int fd);
 char	*ft_strdup(const char *s1);
+
 void	*ft_calloc(size_t count, size_t size);
-t_list	*ft_lstnew(void *content);
+
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+int		ft_putchar_fd(char c, int fd);
+int		ft_putstr_fd(char *s, int fd);
+int		ft_putendl_fd(char *s, int fd);
+int		ft_putnbr_fd(int n, int fd);
 int		ft_putnbr_base_fd(int nbr, char *base, int fd);
 int		ft_putlnbr_fd(long n, int fd);
 #endif
