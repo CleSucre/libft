ac95434d92212e2bd1aa90f19daa60195f5f1182
Julien Thomas <julthoma@3A4.42angouleme.fr>
2023-10-30 22:28:21 +0100

    testing my own function for memmove... not the lib one

--- unitests/tests/test_ft_memmove.c
+++ unitests/tests/test_ft_memmove.c
@@ -8,29 +8,29 @@ int test_ft_memmove(void) {
     char src[] = "Hello, World!";
     char dest[20];
 
-    memmove(dest, src, 7);
+    ft_memmove(dest, src, 7);
     if (ft_memcmp(dest, "Hello, ", 7) != 0)
         return (0);
     ft_memcpy(src, "Hello, World!", 13);
-    memmove(dest, src, 0);
+    ft_memmove(dest, src, 0);
     if (ft_memcmp(dest, "", 0) != 0)
         return (0);
     ft_memcpy(src, "Hello, World!", 13);
-    memmove(src, src + 7, 7);
+    ft_memmove(src, src + 7, 7);
     if (ft_memcmp(src, "World!", 7) != 0)
         return (0);
-    memmove(src + 7, src, 7);
+    ft_memmove(src + 7, src, 7);
     if (ft_memcmp(src, "World!", 7) != 0)
         return (0);
-    memmove(src, src, 7);
+    ft_memmove(src, src, 7);
     if (ft_memcmp(src, "World!", 7) != 0)
         return (0);
     ft_memcpy(src, "Hello, World!", 13);
-    memmove(src, src + 4, 0);
+    ft_memmove(src, src + 4, 0);
     if (ft_memcmp(src, "Hello, World!", 13) != 0)
         return (0);
     ft_memcpy(src, "Hello, World!", 13);
-    memmove(src + 4, src, 0);
+    ft_memmove(src + 4, src, 0);
     if (ft_memcmp(src, "Hello, World!", 13) != 0)
         return (0);
     return (1);
