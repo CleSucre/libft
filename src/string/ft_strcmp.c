8158c43e7128f493f985ea2dac9ceaae577b6a1d
Julien Thomas <julthoma@3A4.42angouleme.fr>
2023-10-30 14:39:09 +0100

    fix Makefile unitests & remove useless code for ft_strcmp

--- src/string/ft_strcmp.c
+++ src/string/ft_strcmp.c
@@ -10,25 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/libft.h"
-
-/*
- * Compare strings
- */
+#include "../includes/libft.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	unsigned char	*cps1;
-	unsigned char	*cps2;
-
-	cps1 = (unsigned char *)s1;
-	cps2 = (unsigned char *)s2;
-	while ((*cps1 || *cps2))
+	while ((*s1 || *s2))
 	{
-		if (*cps1 != *cps2)
-			return (*cps1 - *cps2);
-		cps1++;
-		cps2++;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
 	return (0);
 }
