208f67dc64e8e846628c0008864e2cc9b6f2f6a8
Julien Thomas <julthoma@3G6.42angouleme.fr>
2024-06-24 17:29:16 +0200

    update

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -47,9 +47,17 @@ char	*ft_strjoin_free(char *s1, char *s2)
 	if (!s1 && !s2)
 		return (NULL);
 	if (!s1)
-		return (ft_strdup(s2));
+	{
+		res = ft_strdup(s2);
+		free(s2);
+		return (res);
+	}
 	if (!s2)
-		return (ft_strdup(s1));
+	{
+		res = ft_strdup(s1);
+		free(s1);
+		return (res);
+	}
 	res = ft_strjoin(s1, s2);
 	free(s1);
 	free(s2);
