7ce322b00ae55acbb3f9df232892149acc2589d6
Maxime <pierrot.maxime.49@icloud.com>
2024-07-10 08:27:39 +0200

    add ft_strndup.c

---
+++ src/ft_strndup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpierrot <mpierrot@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 13:12:28 by mpierrot          #+#    #+#             */
+/*   Updated: 2024/07/07 13:12:28 by mpierrot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ *  @brief Duplicate a string with a specific length
+ *  @return char * The duplicate string
+ */
+
+char	*ft_strndup(char *s1, int len)
+{
+	char	*res;
+	int		i;
+
+	res = (char *)ft_calloc(ft_strlen(s1) + 1, sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s1[i] && i < len)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
