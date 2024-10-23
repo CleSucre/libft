7ad767eb048a0a2cb6b7af28fdc66b03f526d740
Maxime <pierrot.maxime.pro@gmail.com>
2024-10-23 23:45:05 +0200

    test

--- src/table/find_table_args.c
+++ src/table/find_table_args.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   find_table_args.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpierrot <mpierrot@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: mpierrot <mpierrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 1970/01/01 01:00:00 by mpierrot          #+#    #+#             */
-/*   Updated: 2024/09/18 10:27:26 by mpierrot         ###   ########.fr       */
+/*   Updated: 2024/10/23 05:09:31 by mpierrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,20 @@
  */
 int	find_table_args(char **table, char *search)
 {
-	int	i;
+	int		i;
+	size_t	search_len;
 
 	i = 0;
+	if (!table || !search)
+		return (-1);
+	search_len = ft_strlen(search);
 	while (table[i])
 	{
-		if (ft_strncmp(table[i], search, ft_strlen(search)) == 0)
-			return (i);
+		if (ft_strncmp(table[i], search, search_len) == 0)
+		{
+			if (table[i][search_len] == 0 || table[i][search_len] == '=')
+				return (i);
+		}
 		i++;
 	}
 	return (-1);
