e0d2a9ef203231ae7b721064eef1066490005e0b
CleSucre <47931504+CleSucre@users.noreply.github.com>
2024-10-24 16:46:32 +0200

    fix ft_tabnew

--- src/table/ft_tabnew.c
+++ src/table/ft_tabnew.c
@@ -15,10 +15,16 @@
 char	**ft_tabnew(size_t size)
 {
 	char	**tab;
+	size_t	i;
 
 	tab = (char **)malloc(sizeof(char *) * (size + 1));
 	if (!tab)
 		return (NULL);
-	tab[size] = NULL;
+	i = 0;
+	while (i < size + 1)
+	{
+		tab[i] = NULL;
+		i++;
+	}
 	return (tab);
 }
